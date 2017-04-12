9e3485f1786e262dcf5ac31dd0fc7b060c51a24f
Ihar Reznichenka <Ihar_Reznichenka@epam.com>
2017-04-12 12:57:48 +0300

    using maven for sonar checking

--- ciSrc/groovy/src/Jenkinsfile.groovy
+++ ciSrc/groovy/src/Jenkinsfile.groovy
@@ -1,41 +1,40 @@
 node('root') {
-   checkout scm
-   String toolJdk = tool name: "jdk8", type: 'hudson.model.JDK'
-   def mvnHome = tool name: 'maven 3.5'
+    String toolJdk = tool name: "jdk8", type: 'hudson.model.JDK'
+    def mvnHome = tool name: 'maven 3.5'
 
-   /* Set JAVA_HOME, and special PATH variables. */
-   List javaEnv = [
-           "PATH+MVN=${toolJdk}/bin:${mvnHome}/bin",
-           "M2_HOME=${mvnHome}",
-           "JAVA_HOME=${toolJdk}"
-   ]
+    /* Set JAVA_HOME, and special PATH variables. */
+    List javaEnv = [
+            "PATH+MVN=${toolJdk}/bin:${mvnHome}/bin",
+            "M2_HOME=${mvnHome}",
+            "JAVA_HOME=${toolJdk}"
+    ]
 
-   withEnv(javaEnv) {
-      stage('Initialize') {
-         sh '''
+    withEnv(javaEnv) {
+        stage('Initialize') {
+            checkout scm
+            sh '''
             echo "PATH = ${PATH}"
             echo "M2_HOME = ${M2_HOME}"
-        '''
-      }
-      try {
-         stage('Build') {
-            sh 'mvn -Dmaven.test.failure.ignore=true install'
-         }
-         stage('SonarQube analysis') {
-            def scannerHome = tool name: 'SonarQube 3.0'
-            withSonarQubeEnv('My SonarQube Server') {
-               sh "${scannerHome}/bin/sonar-scanner"
+          '''
+        }
+        try {
+            stage('Build') {
+                sh 'mvn -Dmaven.test.failure.ignore=true install'
             }
-         }
-      } catch (ex) {
-         currentBuild.result = 'FAILURE'
-         mail to: 'ihar.reznichenka@gmail.com', subject: "Failed Pipeline: ${currentBuild.fullDisplayName}", body: "Something is wrong with ${env.BUILD_URL}"
-         throw ex
-      }
-      stage('Summary') {
-         if (currentBuild.result == null || currentBuild.result == 'SUCCESS') {
-            junit 'target/surefire-reports/**/*.xml'
-         }
-      }
-   }
+            stage('SonarQube analysis') {
+                withSonarQubeEnv('My SonarQube Server') {
+                    sh 'mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.3.0.603:sonar'
+                }
+            }
+        } catch (ex) {
+            currentBuild.result = 'FAILURE'
+            mail to: 'ihar.reznichenka@gmail.com', subject: "Failed Pipeline: ${currentBuild.fullDisplayName}", body: "Something is wrong with ${env.BUILD_URL}"
+            throw ex
+        }
+        stage('Summary') {
+            if (currentBuild.result == null || currentBuild.result == 'SUCCESS') {
+                junit 'target/surefire-reports/**/*.xml'
+            }
+        }
+    }
 }
